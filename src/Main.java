421203febeb43c4d3fd8a1ec440ba92cdee577bd
vivanu <veronika.ivanushko@ab-inbev.com>
2021-10-30 19:26:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public static void main(String[] args) {
+        MessageHandler messageHandler = new MessageAddExclamationMarkHandler(
+                new MessageVerifyHandler(new MessagePrintHandler(null)));
+
+        messageHandler.handle("hi");
+    }
+}
+
+abstract class MessageHandler {
+    MessageHandler messageHandler;
+
+    public MessageHandler(MessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+    }
+
+    abstract void handle(String message);
+}
+
+class MessagePrintHandler extends MessageHandler {
+    public MessagePrintHandler(MessageHandler messageHandler) {
+        super(messageHandler);
+    }
+
+    @Override
+    void handle(String message) {
+        System.out.println(message);
+    }
+}
+
+class MessageVerifyHandler extends MessageHandler {
+    public MessageVerifyHandler(MessageHandler messageHandler) {
+        super(messageHandler);
+    }
+
+    @Override
+    void handle(String message) {
+        if (!message.matches(".*\\d.*")) {
+            messageHandler.handle(message);
+        }
+    }
+}
+
+class MessageAddExclamationMarkHandler extends MessageHandler {
+    public MessageAddExclamationMarkHandler(MessageHandler messageHandler) {
+        super(messageHandler);
+    }
+
+    @Override
+    void handle(String message) {
+        messageHandler.handle(message + "!");
+    }
+}
+
+
